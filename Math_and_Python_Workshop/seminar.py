fb70194d87c4a1ed31b06cea099caaec737f3b9c
Sergey <hydzon@yandex.ru>
2024-02-09 12:25:07 +0900

    Math tasks

--- Math_and_Python_Workshop/seminar.py
+++ Math_and_Python_Workshop/seminar.py
@@ -1,6 +1,9 @@
 """
 1) Сколько слов имеют одинаковые первый и последний сомволы?
 
+2) Отсортировать список так, чтобы сначала сначала шли все слова на X, а потом все остальные
+
+3) Отсортировать список кортежей по пследнему элементу из каждого кортежа
 
 """
 
@@ -14,10 +17,22 @@ def task1(words):
     return count
 
 
+def task2(words_list):
+    words_list_x = [word for word in words_list if word[0] == 'x' or word[0] == 'X']
+    words_list_x += [word for word in words_list if word[0] != 'x' and word[0] != 'X']
+    return words_list_x
+
+
+def task3(tup_list):
+    return sorted(tup_list, key=lambda x: x[-1])
+
+
 # ============================================     MAIN     ===========================================================
 
 def main():
     print(task1(['apple', 'banana', 'aherre', 'a', 'foasf', 'lowrgn4334i80gvfdn', 'a094ngne', 'fomoi320md']))
+    print(task2(['apple', 'foasf', 'Xfsd;ljk', 'lowrgn4334i80gvfdn', 'a094ngne', 'fomoi320md', 'x;ojdfdf']))
+    print(task3([(1, 3), (4, 3, 0), (9, 5, 1)]))
 
 
 if __name__ == '__main__':
--- Math_and_Python_Workshop/tests_for_tasks.py
+++ Math_and_Python_Workshop/tests_for_tasks.py
@@ -1,5 +1,6 @@
 import file_tasks
 import seminar
+import dz_seminar
 
 
 def test_task1():
@@ -8,11 +9,21 @@ def test_task1():
 
 def test_task2():
     assert file_tasks.task2('sheet1.txt') == 'CONTENT'
+    assert seminar.task2(['apple', 'banana', 'aherre', 'a', 'foasf', 'Xfsd;ljk', 'lowrgn4334i80gvfdn', 'a094ngne',
+                          'fomoi320md', 'x;ojdfdf']) == ['Xfsd;ljk', 'x;ojdfdf', 'apple', 'banana', 'aherre', 'a',
+                                                         'foasf', 'lowrgn4334i80gvfdn', 'a094ngne', 'fomoi320md']
 
 
 def test_task3():
     assert file_tasks.task3('Привет, мир!') == 'MESSAGE SAVED'
+    assert seminar.task3([(1, 3), (4, 3, 0), (9, 5, 1)]) == [(4, 3, 0), (9, 5, 1), (1, 3)]
 
 
 def test_task4():
     assert file_tasks.task4('output.txt', 'событие') == 'событие записано'
+
+
+def test_dz_seminar():
+    assert dz_seminar.even_indeces([1, 1, 2, 3, 5, 8, 13, 21, 34]) == [1, 2, 5, 13, 34]
+    assert dz_seminar.even_elements([1, 1, 2, 3, 5, 8, 13, 21, 34]) == [2, 8, 34]
+
