cd14f84d29a30a831ddcc7edeb963a55af5b5be2
Sergey <hydzon@yandex.ru>
2024-02-12 22:41:37 +0900

    Math tasks

--- Math_and_Python_Workshop/elective.py
+++ Math_and_Python_Workshop/elective.py
@@ -9,8 +9,7 @@
 Порядок цифр в ответе не важен.
 
 """
-
-
+from functools import reduce
 # ============================================     tasks  ===========================================================
 from math import *
 
@@ -19,10 +18,7 @@ def permanent_caprecar(n):
     for i in range(7):
         n_sorted = int(''.join(sorted(str(n))))
         n_sorted_rev = int(''.join(sorted(str(n), reverse=True)))
-        if n_sorted > n_sorted_rev:
-            n = n_sorted - n_sorted_rev
-        else:
-            n = n_sorted_rev - n_sorted
+        n = n_sorted_rev - n_sorted
         if n == 6174:
             break
     return n
@@ -33,15 +29,42 @@ def numerics(n):
     return list(map(int, str(n)))
 
 
+def kaprekar_step(L):
+    sort_L = int(''.join(list(map(str, sorted(L)))))
+    rev_L = int(''.join(list(map(str, sorted(L, reverse=True)))))
+    return rev_L - sort_L
 
+
+def kaprekar_loop(n):
+    if not kaprekar_check(n):
+        print(f'Ошибка! На вход подано число {n}, не удовлетворяющее условиям процесса Капрекара')
+    else:
+        print(n)
+        list_n = {n}
+        while n not in (495, 6174, 549945, 631764):
+            n = kaprekar_step(numerics(n))
+            if n in list_n:
+                print(f'Следующее число - {n}, кажется процесс зациклился...')
+                break
+            else:
+                print(n)
+                list_n.add(n)
+
+
+def kaprekar_check(n):
+    if len(numerics(n)) in [3, 4, 6]:
+        if len(set(numerics(n))) != 1:
+            if n not in [100, 1000, 100000]:
+                return True
+    return False
 # ============================================     MAIN     ===========================================================
 
 
 def main():
     plus_infinity = 10000000000
 
-    print(permanent_caprecar(9909))
-    print(numerics(9909))
+    kaprekar_loop(103303)
+
 
 if __name__ == '__main__':
     main()
