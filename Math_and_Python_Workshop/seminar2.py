75560987edbcc1472e2ac11af65d30ca2d1060db
Sergey <hydzon@yandex.ru>
2024-02-19 17:12:55 +0900

    Math tasks

--- Math_and_Python_Workshop/seminar2.py
+++ Math_and_Python_Workshop/seminar2.py
@@ -26,11 +26,29 @@ f(x) в точке x=x0
 Если строка уже содержит 'ing', добавьте 'ly'
 Если длина строки меньше 3, верните строку как есть
 
+
+6)Напишите функцию front_back(a, b), принимающую 2 строки, a и b, и возвращающую строку формы
+a-front + b-front + a-back + b-back
+
+
+7) Напишите функцию mimic_dict(string), которая принимает на вход строковую переменную (может содержать пробелы,
+табуляцию, переносы строк), возвращающую «имитирующий» словарь, который соотносит каждое появившееся слово,
+со списком всех слов, которые следуют за этим словом.
+Для "входа" в словарь используйте в качестве ключа пустую строку, в соответствие которой будет поставлено 1 слово.
+
+
+8) Напишите функцию print_mimic(mimic_dict, word), которая принимает на вход 2 параметра:
+словарь, идентичный тому который возвращает mimic_dict(string) из предыдущей задачи
+строку (1-е слово, с которого надо начать текст)
+
+
 """
 
-# ============================================     tasks  ===========================================================
+# ============================================     tasks 1-5   ===========================================================
 
 from math import *
+import re
+import random
 
 
 def f(x):
@@ -60,13 +78,45 @@ def my_deepcopy(input_list):
     return [my_deepcopy(el) if isinstance(el, list) else el for el in input_list]
 
 
-def verbing(s: str):
+def verbing(s):
     if len(s) < 3:
         return s
     elif s.endswith('ing'):
         return s + 'ly'
     else:
         return s + 'ing'
+
+
+# ============================================     tasks 6-...  ========================================================
+
+
+def front_back(a, b):
+    return a[:round(len(a)/1.9999)] + b[:round(len(b)/1.9999)] + a[round(len(a)/1.9999):] + b[round(len(b)/1.9999):]
+
+
+def mimic_dict(string: str):
+    list_words = re.split(' |\n|    ', string)
+    list_words = [el for el in list_words if len(el) > 0]
+    dict_words = {'': [list_words[0]]}
+    for i in range(len(list_words) - 1):
+        if list_words[i] not in dict_words:
+            s = set()
+            for j in range(len(list_words)):
+                if list_words[j] == list_words[i] and j != len(list_words) - 1:
+                    s.add(list_words[j + 1])
+            dict_words[list_words[i]] = list(s)
+    return dict_words
+
+
+def print_mimic(mimic_dict, word):
+    text = []
+    while len(text) < 20:
+        text.append(word)
+        if word in mimic_dict:
+            word = random.choice(mimic_dict[word])
+        else:
+            word = mimic_dict[''][0]
+    return ' '.join(text)
 
 
 # ============================================     MAIN     ===========================================================
@@ -86,7 +136,12 @@ def main():
     # print(l1)
     # print(l2)
 
-    print(verbing('swiming'))
+    # print(verbing('swiming'))
+
+    # print(front_back('absdc', '11211'))
+    # print(round(5/1.9))
+
+    print(print_mimic(mimic_dict('a cat and a dog\na fly    '), ''))
 
 
 if __name__ == '__main__':
