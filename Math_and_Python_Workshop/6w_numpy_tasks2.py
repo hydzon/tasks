04bc63b06eb42b31f4c34d523f607fdeb6cff04d
Sergey <hydzon@yandex.ru>
2024-02-26 13:52:20 +0900

    new tasks

--- Math_and_Python_Workshop/6w_numpy_tasks2.py
+++ Math_and_Python_Workshop/6w_numpy_tasks2.py
@@ -9,6 +9,16 @@
 Найдите числа, встречающиеся в обоих векторах и составьте их по возрастанию в вектор Z.
 Если пересечений нет, то вектор Z будет пустым.
 
+
+3)  На вход подаётся 2 строки:
+1-я строка содержит 2 натуральных числа: n, m
+2-я строка содержит число k
+
+    Создайте матрицу a размера n*m такую, что каждая её строка содержит числа от k до k+m-1 (с шагом 1).
+
+    Создайте матрицу b размера n*m такую, что каждый её столбец содержит числа от k до k+n-1 (с шагом 1).
+
+
 """
 
 import datetime
@@ -28,12 +38,27 @@ def task1(z):
 def task2(a, b):
     return np.intersect1d(a,  b)
 
+
+def task3(n, m, k):
+    a = np.zeros((n, m), dtype='float') + np.arange(k, m + 1)[:m]
+    b = np.zeros((n, m), dtype='float') + np.arange(k, n + 1)[:n].reshape(n, 1)
+    return print(f'{a} \n\n {b}')
+
 # ============================================     MAIN     ===========================================================
 
 
 def main():
+
     # print(task1(np.array([9.8, 0, -1.2, 0])))
     # print(task2(np.array([0, 9, 7, 1, 3, 7, 5, 2, 5, 1]), np.array([3, 1, 3, 7, 4, 1, 8, 1, 1, 8])))
+    # print(np.arange(np.datetime64('2016-07'), np.datetime64('2016-08'), dtype='datetime64[D]'))
+    # print(np.arange(np.datetime64('2016-07', 'D'), np.datetime64('2016-08', 'D'), dtype=np.datetime64))
+    # task3(5, 4, -5)
+    # print(np.fromiter(range(10), dtype='float'))
+    # print(np.around(np.linspace(0, 1, 10, endpoint=False), decimals=3)[1:])
+    print(np.geomspace(1, 256, 9))
+
+
 
 
 if __name__ == '__main__':
