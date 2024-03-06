ad6ee167dbd887b2fd42d2c5f3a50bdcbe4811a0
Sergey <hydzon@yandex.ru>
2024-03-06 20:32:08 +0900

    new tasks

--- Math_and_Python_Workshop/6w_13_SLAU.py
+++ Math_and_Python_Workshop/6w_13_SLAU.py
@@ -9,6 +9,7 @@
 import numpy as np
 from sys import stdin
 import pandas as pd
+import matplotlib.pyplot as plt
 import re
 from scipy.spatial.distance import cosine
 
@@ -26,6 +27,24 @@ def slau_solution():
         print('Система не имеет решений')
 
 
+def slau_solution2(m, tuple_p):
+    m1 = []
+    v1 = np.array([])
+    for i in range(m):
+        x, y = tuple_p[i]
+        m1.append([x**i for i in range(m)])
+        v1 = np.append(v1, y)
+    m1 = np.array(m1)
+    if np.linalg.det(m1):
+        r = np.linalg.solve(m1, v1)
+        return r
+    else:
+        print('Система не имеет решений')
+
+
+def f(x, c):
+    return c[0] + sum([c[i] * x**i for i in range(1, len(c))])
+
 # ============================================     MAIN     ===========================================================
 
 
@@ -37,7 +56,14 @@ def main():
     # m2 = np.concatenate((m2, [list(map(int, '0 0 1 0 1'.split()))]))
     # print(m2[:, :-1])
 
-    slau_solution()
+    c = slau_solution2(6, ((0, 1), (1, 1), (2, 25), (-1, 1), (-2, -23), (0.5, 0.90625)))
+
+    x = np.arange(-1.3, 0.7, 0.01)  # Массив значений аргумента
+    plt.plot(x, f(x, c))  # Построение графика
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$f(x)$')
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == '__main__':
