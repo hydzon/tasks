79e8e58260b7dd0a3578253f6e1b871ad252cd91
Sergey <hydzon@yandex.ru>
2024-03-13 21:57:49 +0900

    new tasks

--- Math_and_Python_Workshop/9_ MathTasks.py
+++ Math_and_Python_Workshop/9_ MathTasks.py
@@ -5,8 +5,14 @@
 
 Показать минимальное число ходов за которое можно уравнять число бумажек.
 
+
+
+
 """
 
+import numpy as np
+import requests
+import os
 
 # ============================================     tasks 1-5   =========================================================
 
@@ -22,12 +28,41 @@ def task1(inp):
             print(f'За {int((max(inp) * len(inp) - sum(inp)) / 2)} ходов по {(max(inp))} бумажек')
 
 
+def task2():
+    pass
+
 # ============================================     MAIN     ===========================================================
 
 
+class DStr(str):
+    def __init__(self, a):
+        self.a = a
+
+    def __sub__(self, b):
+        return self.a[len(b):] if self.a.startswith(b) else self.a
+
+
 def main():
-    task1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+    # d = DStr('1aaaa')
+    # b = DStr('1')
+    # print(d - b)
+    # print(dir(d))
+    # task1([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+
+    # url = 'https://stepik.org/media/attachments/lesson/254899/menu.json'
+    # resp = requests.get(url)
+    # resp.encoding = 'utf-8'
+    # if resp.status_code == 200:
+    #     print(resp.text)
+    # else:
+    #     print(f'Упс, произошла ошибка!..\nКод ошибки - {resp.status_code}')
+
+
+    print(dir(os.path))
 
 
 if __name__ == '__main__':
     main()
+
+
+
