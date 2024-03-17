019debb31da797f9b617a8af70f28e47857ddeb0
Sergey <hydzon@yandex.ru>
2024-03-17 19:35:39 +0900

    new tasks

--- Basics_Python/Part1.py
+++ Basics_Python/Part1.py
@@ -8,6 +8,8 @@
  - y больше или равно x
  - y делится нацело на 5
 
+ 4) Реализуйте программу, которая для заданных n и k вычисляет C(n, k) - число сочетаний из n по k.
+
 """
 
 # ============================================     tasks 1-5   =========================================================
@@ -26,38 +28,48 @@ def task3(x):
     return x if x % 5 == 0 else x + (5 - x % 5)
 
 
+def task4(n, k):
+    if k == 0:
+        return 1
+    elif k > n:
+        return 0
+    else:
+        return task4(n - 1, k) + task4(n - 1,  k - 1)
+
 # ============================================     MAIN     ===========================================================
 
 
-# def main():
-#     # print(Counter([1, 2, 1, 2, 3, True, 'wef']))
-#     # assert task2([1, 2, 1, 2, 3]) == 3
-#     # assert task2([1, 2, 1, 2, 3, True]) == 4
-#     print(task3(123))
+def main():
+    # print(Counter([1, 2, 1, 2, 3, True, 'wef']))
+    # assert task2([1, 2, 1, 2, 3]) == 3
+    # assert task2([1, 2, 1, 2, 3, True]) == 4
+    # print(task3(123))
+
+    print(task4(10, 5))
+
+
+if __name__ == '__main__':
+    main()
+
+# def s(a, *vs, b=10):
+#     res = a + b
+#     for v in vs:
+#         res += v
+#     print(res)
 #
 #
-# if __name__ == '__main__':
-#     main()
-
-def s(a, *vs, b=10):
-    res = a + b
-    for v in vs:
-        res += v
-    print(res)
-
-
-answers = {
-    's(11, 10)', 's(0, 0, 31)', 's(11, b=20)',
-    's(b=31)', 's(11, 10, b=10)', 's(21)',
-    's(b=31, 0)', 's(11, 10, 10)', 's(5, 5, 5, 5, 1)'
-}
-
-for answer in answers:
-    print(f'функция с аргументами: {answer} | вернула:', end=' ')
-    try:
-        exec(answer)
-    except Exception as e:
-        print(f'ошибку "{type(e)}" с пояснением "{e}"')
+# answers = {
+#     's(11, 10)', 's(0, 0, 31)', 's(11, b=20)',
+#     's(b=31)', 's(11, 10, b=10)', 's(21)',
+#     's(b=31, 0)', 's(11, 10, 10)', 's(5, 5, 5, 5, 1)'
+# }
+#
+# for answer in answers:
+#     print(f'функция с аргументами: {answer} | вернула:', end=' ')
+#     try:
+#         exec(answer)
+#     except Exception as e:
+#         print(f'ошибку "{type(e)}" с пояснением "{e}"')
 
 
 # def tests_for_task2():
