d5dfa1d6a21675e169464c198306cd91dc76513a
Sergey <hydzon@yandex.ru>
2024-03-18 15:50:13 +0900

    new tasks

--- Basics_Python/Part1.py
+++ Basics_Python/Part1.py
@@ -10,6 +10,20 @@
 
  4) Реализуйте программу, которая для заданных n и k вычисляет C(n, k) - число сочетаний из n по k.
 
+ 5) Реализуйте программу, которая будет эмулировать работу с пространствами имен.
+ Необходимо реализовать поддержку создания пространств имен и добавление в них переменных
+
+В первой строке дано число n (1 ≤ n ≤ 100) – число запросов.
+В каждой из следующих n строк дано по одному запросу.
+Запросы выполняются в порядке, в котором они даны во входных данных.
+Имена пространства имен и имена переменных представляют из себя строки длины не более 10,
+состоящие из строчных латинских букв.
+
+Для каждого запроса get выведите в отдельной строке его результат.
+
+
+6) Реализуйте класс Buffer, который будет накапливать в себе элементы последовательности и выводить
+сумму пятерок последовательных элементов по мере их накопления.
 """
 
 # ============================================     tasks 1-5   =========================================================
@@ -36,6 +50,77 @@ def task4(n, k):
     else:
         return task4(n - 1, k) + task4(n - 1,  k - 1)
 
+
+class NameSpace:
+    def __init__(self, name, parent=None):
+        self.name = name
+        self.parent = parent
+        self.variables = []
+
+    def add_variable(self, var):
+        self.variables.append(var)
+
+    def get_variables(self, var):
+        if var in self.variables:
+            return self.name
+        else:
+            if self.parent is None:
+                return None
+            return self.parent.get_variables(var)
+
+
+name_space = {'global': NameSpace('global', None)}
+get_stack = []
+
+
+def create_namespace(new_namespaces, parent):
+    name_space[new_namespaces] = NameSpace(new_namespaces, name_space[parent])
+
+
+def add_variables(namespaces, variable):
+    name_space[namespaces].add_variable(variable)
+
+
+def instructions_implementer(cmd, ns, var):
+    if cmd == 'create':
+        create_namespace(ns, var)
+    elif cmd == 'add':
+        add_variables(ns, var)
+    else:
+        get_stack.append((ns, var))
+
+
+def task5():
+    instructions_count = 9
+    for _ in range(instructions_count):
+        instructions_implementer(*input().split())
+    for i in range(len(get_stack)):
+        print(name_space[get_stack[i][0]].get_variables(get_stack[i][1]))
+
+
+# ============================================     tasks 6-10   ========================================================
+
+
+class Buffer:
+    buffer = []
+
+    def __init__(self):
+        pass
+
+    def add(self, *a):
+        self.buffer.extend(a)
+        for i in range(len(self.buffer) // 5):
+            print(sum(self.buffer[i * 5: i * 5 + 5]))
+        c = len(self.buffer) % 5
+        if len(self.buffer) % 5:
+            self.buffer = self.buffer[-(len(self.buffer) % 5):]
+        else:
+            self.buffer = []
+
+    def get_current_part(self):
+        return self.buffer
+
+
 # ============================================     MAIN     ===========================================================
 
 
@@ -44,8 +129,30 @@ def main():
     # assert task2([1, 2, 1, 2, 3]) == 3
     # assert task2([1, 2, 1, 2, 3, True]) == 4
     # print(task3(123))
+    # print(task4(10, 5))
 
-    print(task4(10, 5))
+    task5()
+    """
+    add global a
+    create foo global
+    add foo b
+    get foo a
+    get foo c
+    create bar foo
+    add bar a
+    get bar a
+    get bar b
+    """
+
+    # buf = Buffer()
+    # buf.add(1, 2, 3)
+    # print(buf.get_current_part())  # вернуть [1, 2, 3]
+    # buf.add(4, 5, 6)  # print(15) – вывод суммы первой пятерки элементов
+    # print(buf.get_current_part())  # вернуть [6]
+    # buf.add(7, 8, 9, 10)  # print(40) – вывод суммы второй пятерки элементов
+    # print(buf.get_current_part())  # вернуть []
+    # buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)  # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+    # print(buf.get_current_part())  # вернуть [1]
 
 
 if __name__ == '__main__':
