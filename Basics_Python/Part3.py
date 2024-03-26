297bdcda30e5e2d28715f87ac6d13f951d610493
Sergey <hydzon@yandex.ru>
2024-03-26 18:40:45 +0900

    new tasks

--- Basics_Python/Part3.py
+++ Basics_Python/Part3.py
@@ -6,7 +6,22 @@
 
 2) Выведите одно число – количество вхождений строки t в строку s.
 
+3) Вам дана последовательность строк.
+Выведите строки, содержащие "cat" в качестве подстроки хотя бы два раза.
+
+4) Вам дана последовательность строк.
+Выведите строки, содержащие "cat" в качестве слова.
+
+5) Вам дана последовательность строк.
+Выведите строки, содержащие две буквы "z", между которыми ровно три символа.
+
+6) Вам дана последовательность строк.
+Выведите строки, содержащие слово, состоящее из двух одинаковых частей (тандемный повтор).
+
 """
+
+import sys
+import re
 
 
 # ============================================     tasks 1-2   =========================================================
@@ -34,6 +49,35 @@ def task2(s: str, t: str):
     return print(len([True for i in range(len(s)) if s[i:].startswith(t)]))
 
 
+def task3():
+    for line in sys.stdin:
+        line = line.rstrip()
+        if re.findall(r'cat{,2}', line):
+            print(line)
+
+
+def task4():
+    for line in sys.stdin:
+        line = line.rstrip()
+        re_obj = re.search(r'\bcat\b', line)
+        if re_obj:
+            print(line)
+
+
+def task5():
+    for line in sys.stdin:
+        line = line.rstrip()
+        re_obj = re.search(r'z\w\w\wz', line)
+        if re_obj:
+            print(line)
+
+
+def task6():
+    for line in sys.stdin:
+        line = line.rstrip()
+        if re.search(r'\b(\w+)\1\b', line):
+            print(line)
+
 # ============================================     MAIN     ===========================================================
 
 
@@ -42,9 +86,15 @@ def main():
     # s, a, b = input(), input(), input()
     # task1(s, a, b)
 
-    s = "aabaaa"
-    t = "aa"
-    task2(s, t)
+    # s = "aabaaa"
+    # t = "aa"
+    # task2(s, t)
+
+    # task3()
+    # task4()
+    # task5()
+    # print(re.match(r'\bcat\b', 'cat cat cat'))
+
 
 
 if __name__ == '__main__':
